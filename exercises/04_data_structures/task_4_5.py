f32715a659f106c41b46b37f20b18f632b59894a
5vetlana <svetlana.shetinina@gmail.com>
2022-01-09 13:54:50 -0600

    Task 4.5 Completed

--- exercises/04_data_structures/task_4_5.py
+++ exercises/04_data_structures/task_4_5.py
@@ -22,3 +22,11 @@ this stage it is difficult otherwise test the result.
 
 command1 = "switchport trunk allowed vlan 1,2,3,5,8"
 command2 = "switchport trunk allowed vlan 1,3,8,9"
+
+set1 = set(command1.split()[-1].split(','))
+set2 = set(command2.split()[-1].split(','))
+#Get a list of vlans in both set1 and set2
+result = list(set1.intersection(set2))
+#Sort list in ascending order
+result.sort()
+print(result)
