54d70fe96ac16d7855d709605e16b20b5aea43bb
5vetlana <svetlana.shetinina@gmail.com>
2022-01-09 20:04:30 -0600

    Task 4.8 Completed

--- exercises/04_data_structures/task_4_8.py
+++ exercises/04_data_structures/task_4_8.py
@@ -26,3 +26,11 @@ this stage it is difficult otherwise test the result.
 """
 
 ip = "192.168.3.1"
+ip = ip.split('.')
+oct1, oct2, oct3, oct4 = int(ip[0]), int(ip[1]), int(ip[2]), int(ip[3])
+
+ip_template = '''
+{0:<8}  {1:<8}  {2:<8}  {3:<8}
+{0:<08b}  {1:<08b}  {2:<08b}  {3:<08b}
+'''
+print(ip_template.format(oct1, oct2, oct3, oct4))
