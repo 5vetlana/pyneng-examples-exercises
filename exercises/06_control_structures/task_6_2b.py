28b2d5bf826ea9bfb9c1c566c003a530f9f7f541
5vetlana <svetlana.shetinina@gmail.com>
2022-01-16 14:13:12 -0600

    Completed Task 6.2b

--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -8,3 +8,38 @@ Add this functionality: If the address was entered incorrectly, request the addr
 
 Restriction: All tasks must be done using the topics covered in this and previous chapters.
 """
+valid_ip = False
+
+while not valid_ip:
+
+    try:
+        ip_address = input("Enter an IP address in the format 10.1.1.1: ")
+        ip_addr = ip_address.split('.')
+
+
+        valid_ip = len(ip_addr) == 4
+
+        for num in ip_addr:
+            valid_ip = num.isdigit() and 0 <= int(num) <= 255 and valid_ip
+
+        oct1 = ip_addr[0]
+        oct1 = int(oct1)
+
+    #For exception wlhere "." is not used as a separeator
+    except:
+        print("Invalid IP Address")
+
+    else:
+        if valid_ip:
+            if 1 <= oct1 <= 223:
+                print("unicast")
+            elif 224 <= oct1 <= 239:
+                print("multicast")
+            elif ip_address == "255.255.255.255":
+                print('local broadcast')
+            elif ip_address == "0.0.0.0":
+                print('unassiged')
+            else:
+                print('unused')
+        else:
+            print("Invalid IP Addresss")
