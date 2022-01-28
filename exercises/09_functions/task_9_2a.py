4511a97dd6f80c07fd0d06d78ceb7bb4a8908607
5vetlana <svetlana.shetinina@gmail.com>
2022-01-28 14:56:09 -0600

    Completed Task 9.2a

--- exercises/09_functions/task_9_2a.py
+++ exercises/09_functions/task_9_2a.py
@@ -46,3 +46,32 @@ trunk_config = {
     "FastEthernet0/2": [11, 30],
     "FastEthernet0/4": [17],
 }
+
+def generate_trunk_config(intf_vlan_mapping, trunk_template):
+    """
+    intf_vlan_mapping: expects a dictionary with interface-VLAN mapping:
+        {'FastEthernet0/1': [10, 20],
+        'FastEthernet0/2': [11, 30],
+        'FastEthernet0/4': [17]}
+    trunk_template: expects trunk port configuration template as command list
+        (trunk_mode_template list)
+
+    Return a list of commands with configuration based on the specified ports and trunk_mode_template.
+    """
+    command_dict = {}
+    interface_trunk_list = []
+
+    for key, value in intf_vlan_mapping.items():
+        for command in trunk_template:
+            if command == "switchport trunk allowed vlan":
+                str_vlans = [str(vlan) for vlan in value]
+                vlans = ",".join(str_vlans)
+                interface_trunk_list.append(command + " " + vlans)
+            else:
+                interface_trunk_list.append(command)
+
+        command_dict[key] = interface_trunk_list
+        interface_trunk_list = []
+    return command_dict
+
+print(generate_trunk_config(trunk_config, trunk_mode_template))
