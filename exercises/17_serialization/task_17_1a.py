9a631da118d552516ed9508bbadf96aea7297257
5vetlana <svetlana.shetinina@gmail.com>
2022-02-27 09:52:32 -0600

    Renamed Task 17.1b to 17.1a

---
+++ exercises/17_serialization/task_17_1a.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Task 17.1
+
+Create the write_dhcp_snooping_to_csv function, which processes the output
+of the show dhcp snooping binding command from different files and writes
+the processed data to the csv file.
+
+Function arguments:
+* filenames - list of filenames with "show dhcp snooping binding" command output
+* output - the name of the csv file into which the result will be written
+
+The function returns None.
+
+For example, if a list with one file sw3_dhcp_snooping.txt was passed as an argument:
+MacAddress          IpAddress        Lease(sec)  Type           VLAN  Interface
+------------------  ---------------  ----------  -------------  ----  --------------------
+00:E9:BC:3F:A6:50   100.1.1.6        76260       dhcp-snooping   3    FastEthernet0/20
+00:E9:22:11:A6:50   100.1.1.7        76260       dhcp-snooping   3    FastEthernet0/21
+Total number of bindings: 2
+
+The resulting csv file should contain the following content:
+switch,mac,ip,vlan,interface
+sw3,00:E9:BC:3F:A6:50,100.1.1.6,3,FastEthernet0/20
+sw3,00:E9:22:11:A6:50,100.1.1.7,3,FastEthernet0/21
+
+The first column in the csv file, the name of the switch, must be obtained from
+the file name, the rest - from the contents in the files.
+
+Check the function on the contents of the files sw1_dhcp_snooping.txt,
+sw2_dhcp_snooping.txt, sw3_dhcp_snooping.txt.
+"""
+import csv
+import re
+
+def write_dhcp_snooping_to_csv(filenames, output):
+    line = {}
+    data = []
+    for file in filenames:
+        with open(file, 'r') as src:
+            regex = re.compile(r'(?P<mac>[\S*:]+)\s*(?P<ip>[\d+.]+)\s*\d+\s*\S+\s*(?P<vlan>\d*)\s*(?P<intf>\w+\d/\d+)\n')
+            for match in regex.finditer(src.read()):
+                line['switch'] = file.split('_')[0]
+                line['mac'] = match.group('mac')
+                line['ip'] = match.group('ip')
+                line['vlan'] = match.group('vlan')
+                line['interface'] = match.group('intf')
+                data.append(line)
+                line = {}
+
+
+    with open(output, 'w', newline = '') as dest:
+        writer = csv.DictWriter(
+        dest, fieldnames=list(data[0].keys()), quoting=csv.QUOTE_NONNUMERIC)
+        writer.writeheader()
+        for row in data:
+            writer.writerow(row)
